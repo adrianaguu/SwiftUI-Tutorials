2e3c44f18c97bf31997dab39ffebd76528830c13
Adriana Gutierrez <adriana.gutierrez@ucsp.edu.pe>
2021-10-22 16:11:04 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Adriana Gutierrez on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
